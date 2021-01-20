b73d82b1240057dc6292b7ad4324c952421cfc99
Ash Foster <ajp.foster@gmail.com>
2021-01-20 20:02:45 +1300

    +add mongo -changes to MVC and CRUD

---
+++ dotnetusTodoysys/Controllers/TodosController.cs
@@ -0,0 +1,74 @@
+﻿using TodosApi.Models;
+using TodosApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace TodosApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodosController : ControllerBase
+    {
+        private readonly TodoService _todoService;
+
+        public TodosController(TodoService todoService)
+        {
+            _todoService = todoService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Todo>> Get() =>
+            _todoService.Get();
+
+        [HttpGet("{id:length(24)}", Name = "GetTodo")]
+        public ActionResult<Todo> Get(string id)
+        {
+            var todo = _todoService.Get(id);
+
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            return todo;
+        }
+
+        [HttpPost]
+        public ActionResult<Todo> Create(Todo todo)
+        {
+            _todoService.Create(todo);
+
+            return CreatedAtRoute("GetTodo", new { id = todo.Id.ToString() }, todo);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, Todo todoIn)
+        {
+            var todo = _todoService.Get(id);
+
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            _todoService.Update(id, todoIn);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult Delete(string id)
+        {
+            var todo = _todoService.Get(id);
+
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            _todoService.Remove(todo.Id);
+
+            return NoContent();
+        }
+    }
+}
---
+++ dotnetusTodoysys/Services/TodoService.cs
@@ -0,0 +1,42 @@
+﻿
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Linq;
+using TodosApi.Models;
+
+namespace TodosApi.Services
+{
+    public class TodoService
+    {
+        private readonly IMongoCollection<Todo> _todos;
+
+        public TodoService(ITodoDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _todos = database.GetCollection<Todo>(settings.TodosCollectionName);
+        }
+
+        public List<Todo> Get() =>
+            _todos.Find(todo => true).ToList();
+
+        public Todo Get(string id) =>
+            _todos.Find(todo => todo.Id == id).FirstOrDefault();
+
+        public Todo Create(Todo todo)
+        {
+            _todos.InsertOne(todo);
+            return todo;
+        }
+
+        public void Update(string id, Todo todoIn) =>
+            _todos.ReplaceOne(todo => todo.Id == id, todoIn);
+
+        public void Remove(Todo todoIn) =>
+            _todos.DeleteOne(todo => todo.Id == todoIn.Id);
+
+        public void Remove(string id) =>
+            _todos.DeleteOne(todo => todo.Id == id);
+    }
+}
